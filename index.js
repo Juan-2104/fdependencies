3672565cdf4ea94b4d60e63c5e4c734c359704e8
Camilo González <juangonzaleznegocios@gmail.com>
2023-09-14 15:24:30 -0500

    Export fixed Logger to logger

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const dotenv = require("dotenv");
 dotenv.config();
 
-const { Logger } = require("./src/dependencies/bei-logger");
+const logger = require("./src/dependencies/bei-logger");
 const { EncryptData, DecryptData } = require("./src/dependencies/crypto-utils");
 const { isJson, serializeValues, serializeValue } = require("./src/dependencies/json-eval");
 const { NotAuthorizedError, ValidaAPIKey } = require("./src/dependencies/secutils");
 const { sendSuccess, sendError, sendCustomResponse } = require("./src/dependencies/handler");
 
 module.exports = {
-  Logger,
+  logger,
   EncryptData,
   DecryptData,
   isJson,
